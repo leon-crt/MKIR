5b442a3a6914a72a7f139e216aa9a75e581a3ce9
leon <leon.pirazzoli@studio.unibo.it>
2023-03-29 21:15:00 +0200

    configurato progetto dune

---
+++ indexer/_build/default/bin/main.ml
@@ -0,0 +1,11 @@
+let file = "/home/leon/Tesi/Dedukti/examples/append.dk"
+let () = 
+        let ic = open_in file in 
+        try
+                let line = input_line ic in
+                print_endline line;
+                flush stdout;
+                close_in ic
+        with e ->
+                close_in_noerr ic;
+                raise e
